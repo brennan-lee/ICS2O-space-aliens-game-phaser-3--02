389d7f694b018885aa05cd90b5994be834556c12
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-10 17:09:55 -0500

    change

--- js/game.js
+++ js/game.js
@@ -1,33 +1,20 @@
 /* global Phaser */
-
+ 
 // Copyright (c) 2022 Brennan Lee All rights reserved
 //
 // Created by: Brennan Lee
 // Created on: nov 2022
 // This is the phaser3 game configuration file
-
+ 
 /**
- * start phaser game.
- */
- const config = {
-   type: Phaser.AUTO,
-   width: 1920,
-   height: 1080,
-   physics: {
-     default: "arcade",
-     arcade: {
-       debug: true
-    },
-  },
-    //set background color
-   backgroundColor: 0x5f6e7a,
-  scale: {
-      mode: phaser.scale.FIT,
-      //we replaced it in the middle of the page
-       autocenter: phaser.scale.CENTER_BOTH
-    },
-},
-
-  const game = new Phaser.Game(config)
-  console.log(game)
-  
+* start phaser game.
+*/
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+ };
+ const game = new Phaser.Game(config);
+ console.log(game);
+ 
